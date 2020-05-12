213558d4288a541cffd942952034de7b0747d183
Sam Popowich <spopowich@gmail.com>
2020-05-12 13:48:10 -0600

    Update.

--- sh_test.rb
+++ sh_test.rb
@@ -1,7 +1,5 @@
-# TEST Summary Holdings code
-#
 def overlap?(a, b)
-  a.include?(b.begin) || b.include?(a.begin) || a.end==b.begin || a.end+1==b.begin
+  a.include?(b.begin) || b.include?(a.begin) || a.end==b.begin || a.end+1==b.begin #this last to cover the fact that years have thickness
 end
 
 def merge_ranges(a, b) # where ranges overlap
@@ -14,49 +12,37 @@ end
 
 def clean(summaryHoldings)
   summaryHoldings.each{ |range|
-    range.gsub!(/\(/,"",).gsub!(/\)/,"").strip! if range.include?("(")
+    range.gsub(/\(/,"",).gsub(/\)/,"").strip if range.include?("(")
   }
 end
 
-def pp(range)
-  if range.end == Time.now.year then
-    endr = "present"
-  else
-    endr = range.end.to_s
-  end
-  return range.begin.to_s+"-"+endr
+def pp(range)  # pretty-print an individual range
+  (range.end == Time.now.year) ? endr = "present" : endr = range.end.to_s
+  range.begin.to_s+"-"+endr
 end
 
-def pretty_print(holdings)
+def pretty_print(holdings) # pretty-print the whole summary_holdings statement
   range, combined, message = holdings
+  statement = ""
   if combined.empty?
-    if range.class == Range
-      return pp(range)+", #{message}"
-    elsif range.class == Array
-      combined = ""
-      range.each{ |r|
-        combined+=pp(r)+", "
-      }
-      return combined+", #{message}"
-    end
+    statement = pp(range)
   else # combined not empty
     pretty = ""
     combined.each { |a|
       pretty += pp(a)
     }
-    return pretty_print([range, [], message])+pretty
+    statement = pretty_print([range, [], message])+pretty
   end
+  statement+", #{message}"
 end
 
 def compile(summaryHoldings)
   thresholds = []
 
   clean(summaryHoldings).each{ |range|
-    startr, message = nil, nil
-    endr = Time.now.year
     range_scan = range.scan(/\d{4}/)
     startr = range_scan.first.to_i
-    endr = range_scan.last.to_i if ((range_scan.count == 2) or range.scan(/-/).empty?)
+    (range_scan.count == 2 or range.scan(/-/).empty?) ? endr = range_scan.last.to_i : endr = Time.now.year
     message = range.split("-").last if (range_scan.count==1 and range.split("-").count == 2)
     thresholds << {:range =>(startr...endr), :message=>message}
   }
@@ -67,21 +53,41 @@ def merge(summaryHoldings)
   merged = summaryHoldings.first[:range]
   message = ""
   combined = []
+
+  # merge overlapping ranges
   summaryHoldings.each { |holdings|
     merged = merge_ranges(merged, holdings[:range]) if overlap?(merged, holdings[:range])
-    message = holdings[:message] if holdings[:message]
+    message = holdings[:message] if holdings[:message] # this only needs to be done once
   }
+
+  # combine disjoint ranges
   summaryHoldings.each{ |holdings|
     combined << combine_ranges(merged, holdings[:range]) unless overlap?(merged, holdings[:range])
   }
-  # merged and combined will always include the *first* range (see
-  # initial assignment to merged)
-  return merged, combined.flatten.drop(1), message
+
+  return merged, combined.flatten.drop(1), message  # drop(1) to get rid of the duplicated first range 
 end
 
-
+# all overlap, with embargo (embargo superseded by "present"): not
+# working, should be: 1956-present
 summaryHoldings = ["(1956)-(1965)", "(1966)-(1999)", "(1990)-most recent 3 years unavailable", "(1995)", "(2000)-"]
 puts pretty_print(merge(compile(summaryHoldings)))
-puts "======="
+# all overlap, with correct embargo: this works.
+summaryHoldings = ["(1956)-(1965)", "(1966)-(1999)", "(1990)-most recent 3 years unavailable", "(1995)", "(2000)"]
+puts pretty_print(merge(compile(summaryHoldings)))
+# none overlap/disjoint closed: this works (trailing comma)
 summaryHoldings = ["(1956)-(1960)", "(1966)-(1999)"]
+puts pretty_print(merge(compile(summaryHoldings)))
+# disjoint, open-ended: this works (trailing comma)
+summaryHoldings = ["(1956)-(1960)", "(1966)-"]
+puts pretty_print(merge(compile(summaryHoldings)))
+# overlap closed: this works
+summaryHoldings = ["(1966)-(1999)", "(1990)-(2010)"]
+puts pretty_print(merge(compile(summaryHoldings)))
+# one disjoint, one overlap: not working, should be 1956-1960, 1966-2010
+summaryHoldings = ["(1956)-(1960)", "(1966)-(1999)", "(1990)-(2010)"]
+puts pretty_print(merge(compile(summaryHoldings)))
+# one disjoint, one overlap, open-ended: not working, should be
+# 1956-1960, 1966-present
+summaryHoldings = ["(1956)-(1960)", "(1966)-(1999)", "(1990)-"]
 puts pretty_print(merge(compile(summaryHoldings)))
